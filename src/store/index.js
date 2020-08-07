2e6d287a310a7d8b18ae550c72735b7b27539c38
Bryan Garza <lagarzab@gmail.com>
2020-08-07 16:20:54 -0400

    Added state functionality

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,36 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    account: {},
+    user: {},
   },
   mutations: {
+    SET_ACCOUNT_DATA(state, payload) {
+      state.account = payload;
+    },
+    SET_USER_DATA(state, payload) {
+      state.user = payload;
+    },
   },
   actions: {
+    setAccountData({ commit }, payload) {
+      console.log(payload);
+      return new Promise((resolve) => {
+        commit('SET_ACCOUNT_DATA', payload);
+        resolve();
+      });
+    },
+    setUserData({ commit }, payload) {
+      console.log(payload);
+      return new Promise((resolve) => {
+        commit('SET_USER_DATA', payload);
+        resolve();
+      });
+    },
+  },
+  getters: {
+    getAccount: (state) => state.account,
+    getUser: (state) => state.user,
   },
   modules: {
   },
