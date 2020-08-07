c113134e2d4aad1fb418c596b6a0d45cb9aa0194
Bryan Garza <lagarzab@gmail.com>
2020-08-07 18:25:39 -0400

    Added methods to pull in data from vuex

--- src/views/Poc.vue
+++ src/views/Poc.vue
@@ -4,20 +4,30 @@
     <br>
     {{ status }}
     <br>
+    <br>
     <h2>This is testing if the injected data can be found on this page.</h2>
     <p>
       This is a test of the injected data's username:
-      <strong>{{ 'nothing found. :(' }}</strong>
+      <strong>{{ getUser.userName || 'nothing found. :(' }}</strong>
+    </p>
+    <p>
+      This is a test of the injected data's account.number:
+      <strong>{{ getAccount.number|| 'nothing found. :(' }}</strong>
     </p>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
   data() {
     return {
       status: 'Sending request...',
     };
+  },
+  computed: {
+    ...mapGetters(['getUser', 'getAccount']),
   },
 };
 </script>
