3f81a2f62897fed1ac0b3cbb9b003336da0336ba
Bryan Garza <lagarzab@gmail.com>
2020-08-07 16:21:44 -0400

    Added configuration files

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  publicPath: '',
+  outputDir: 'dist',
+  assetsDir: '',
+  indexPath: 'index.html',
+  filenameHashing: true,
+  runtimeCompiler: false,
+  transpileDependencies: [],
+  productionSourceMap: true,
+  parallel: true,
+  pages: undefined,
+  crossorigin: undefined,
+  integrity: false,
+  css: {},
+  lintOnSave: 'default',
+  devServer: {}
+}
