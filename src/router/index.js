c5290acdad75ac964ca7dce0e98e034aad07c2f3
Bryan Garza <lagarzab@gmail.com>
2020-08-07 18:06:29 -0400

    Created proof of concept page/route

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
+  {
+    path: '/poc',
+    name: 'Proof of Concept',
+    // route level code-splitting
+    // this generates a separate chunk (poc.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "poc" */ '../views/Poc.vue'),
+  },
 ];
 
 const router = new VueRouter({
---
+++ src/views/Poc.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="ofac">
+    <h1>This page is used to test an API call.</h1>
+    <br>
+    {{ status }}
+    <br>
+    <h2>This is testing if the injected data can be found on this page.</h2>
+    <p>
+      This is a test of the injected data's username:
+      <strong>{{ 'nothing found. :(' }}</strong>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      status: 'Sending request...',
+    };
+  },
+};
+</script>
